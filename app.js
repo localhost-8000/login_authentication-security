00f2c283673290e4d1875a1955ae108a79f67ba4
Rahul Tiwari <rahultwr0005@gmail.com>
2021-02-27 11:14:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.post("/register", (req, res) => {
 
 app.post("/login", (req, res) => {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne( {email: username}, (err, foundUser) => {
         if (err) {
